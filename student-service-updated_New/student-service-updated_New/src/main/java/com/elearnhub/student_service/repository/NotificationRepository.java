12013ba58260a12605f9f1d266ba00628ac02ad8
nama182005 <bhadanenamrata18@gmail.com>
2026-01-10 13:52:02 +0530

    Update project - deleted & added files

--- student-service-updated_New/student-service-updated_New/src/main/java/com/elearnhub/student_service/repository/NotificationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.elearnhub.student_service.repository;
-
-import com.elearnhub.student_service.entity.Notification;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NotificationRepository extends JpaRepository<Notification, Long> {
-}
--- student-service-updated_New/student-service-updated_New/src/main/java/com/elearnhub/student_service/repository/SubmissionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.elearnhub.student_service.repository;
-
-import com.elearnhub.student_service.entity.Submission;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SubmissionRepository extends JpaRepository<Submission, Long> {
-}
